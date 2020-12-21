fa569c662e3b6ffc37ba97c28436c7257127faac
mattcullenmeyer <mattcullenmeyer@gmail.com>
2020-12-21 13:16:49 -0700

    Started nav header

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import HomeView, AboutView 
 
+app_name = 'pages'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
