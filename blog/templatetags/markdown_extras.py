080cfad28bbde2825ccfaae8c95752ab6650051d
mattcullenmeyer <mattcullenmeyer@gmail.com>
2020-12-27 13:37:46 -0700

    Added syntax highlighting to blog

--- blog/templatetags/markdown_extras.py
+++ blog/templatetags/markdown_extras.py
@@ -1,12 +1,16 @@
 from django import template
 from django.template.defaultfilters import stringfilter
-
+from django.utils.safestring import mark_safe
 import markdown as md
 
 register = template.Library()
-
-
-@register.filter()
+@register.filter(is_safe=False)
 @stringfilter
+
 def markdown(value):
-    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+    return mark_safe(md.markdown(
+        value,
+        # codehilite adds classes to code; 3 fenced_code allows you to use ``` instead of #! with indent
+        extensions = ['markdown.extensions.codehilite', 'markdown.extensions.fenced_code', ],
+        safe_mode=False,
+        enable_attributes=False))
