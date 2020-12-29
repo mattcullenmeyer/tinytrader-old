0edab4a4d8606038c9996e38bdef18a521362925
mattcullenmeyer <mattcullenmeyer@gmail.com>
2020-12-29 13:46:30 -0700

    Added tests for pages and users apps

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import HomeView, AboutView
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('pages:home') # reverse returns the actual url of the url pattern
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'pages/home.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.')
+
+    # checks that the name of the view used to resolve "/" matches HomeView
+    def test_homepage_url_resolves_homepageview(self): 
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomeView.as_view().__name__
+        )
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('pages:about')
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, 'pages/about.html')
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, 'About')
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, 'Hi there! I should not be on the page.')
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve('/about/')
+        self.assertEqual(
+            view.func.__name__,
+            AboutView.as_view().__name__
+        )
