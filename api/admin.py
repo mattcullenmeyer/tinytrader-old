bce8c96b6a0a43e251ed438a0548db9deb34b86e
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-01-04 18:52:23 -0700

    Recreated API model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api import models
 
-# Register your models here.
+admin.site.register(models.Ticker)
+admin.site.register(models.Sector)
+admin.site.register(models.Industry)
+admin.site.register(models.MarketCapSize)
+admin.site.register(models.Metadata)
+admin.site.register(models.Metric)
