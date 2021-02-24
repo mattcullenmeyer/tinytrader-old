4778dba534850baafb5b16ed17478e4a082b0524
mattcullenmeyer <mattcullenmeyer@gmail.com>
2021-02-24 16:33:05 -0700

    Simplified JS with function to update ranks

--- static/js/apps/ranking.js
+++ static/js/apps/ranking.js
@@ -17,120 +17,29 @@ fetch(`/api/ticker/${ticker}/`)
 		//console.log(result)
 
         // Overall Rank
-		document.getElementById("comp-rank").innerHTML = result.composite_rank;
-        document.getElementById("comp-rank-arc-1").style.borderColor = first_arc_border_color(result.composite_rank);
-        document.getElementById("comp-rank-arc-2").style.borderColor = second_arc_border_color(result.composite_rank);
-        document.getElementById("comp-rank-arc-2").style.transform =
-            "rotate(" + second_arc_transform(result.composite_rank) + "deg)";
-
-        if (result.composite_rank === null) {
-            document.getElementById("comp-rank").innerHTML = null_value;
-            document.getElementById("comp-rank").style.color = null_text_color;
-            document.getElementById("comp-rank-arc-1").style.borderColor = null_border_color;
-            document.getElementById("comp-rank-arc-2").style.borderColor = null_border_color;
-        }
+        updateRank(result.composite_rank, "comp-rank", "comp-rank-arc-1", "comp-rank-arc-2");
 
         // Value Rank
-        document.getElementById("value-rank").innerHTML = result.value_rank;
-        document.getElementById("value-rank-arc-1").style.borderColor = first_arc_border_color(result.value_rank);
-        document.getElementById("value-rank-arc-2").style.borderColor = second_arc_border_color(result.value_rank);
-        document.getElementById("value-rank-arc-2").style.transform =
-            "rotate(" + second_arc_transform(result.value_rank) + "deg)";
-            
-        if (result.value_rank === null) {
-            document.getElementById("value-rank").innerHTML = null_value;
-            document.getElementById("value-rank").style.color = null_text_color;
-            document.getElementById("value-rank-arc-1").style.borderColor = null_border_color;
-            document.getElementById("value-rank-arc-2").style.borderColor = null_border_color;
-        }
+        updateRank(result.value_rank, "value-rank", "value-rank-arc-1", "value-rank-arc-2");
 
         // Momentum Rank
-        document.getElementById("mom-rank").innerHTML = result.mom_rank;
-        document.getElementById("mom-rank-arc-1").style.borderColor = first_arc_border_color(result.mom_rank);
-        document.getElementById("mom-rank-arc-2").style.borderColor = second_arc_border_color(result.mom_rank);
-        document.getElementById("mom-rank-arc-2").style.transform =
-            "rotate(" + second_arc_transform(result.mom_rank) + "deg)";
-            
-        if (result.mom_rank === null) {
-            document.getElementById("mom-rank").innerHTML = 'n/a';
-            document.getElementById("mom-rank").style.color = '#898989';
-            document.getElementById("mom-rank-arc-1").style.borderColor = 'rgb(248,250,252)';
-            document.getElementById("mom-rank-arc-2").style.borderColor = 'rgb(248,250,252)';
-        }
-            
+        updateRank(result.mom_rank, "mom-rank", "mom-rank-arc-1", "mom-rank-arc-2");
             
         // Quality Rank
-        document.getElementById("quality-rank").innerHTML = result.quality_rank;
-        document.getElementById("quality-rank-arc-1").style.borderColor = first_arc_border_color(result.quality_rank);
-        document.getElementById("quality-rank-arc-2").style.borderColor = second_arc_border_color(result.quality_rank);
-        document.getElementById("quality-rank-arc-2").style.transform =
-            "rotate(" + second_arc_transform(result.quality_rank) + "deg)";
-            
-        if (result.quality_rank === null) {
-            document.getElementById("quality-rank").innerHTML = 'n/a';
-            document.getElementById("quality-rank").style.color = '#898989';
-            document.getElementById("quality-rank-arc-1").style.borderColor = 'rgb(248,250,252)';
-            document.getElementById("quality-rank-arc-2").style.borderColor = 'rgb(248,250,252)';
-        }
+        updateRank(result.quality_rank, "quality-rank", "quality-rank-arc-1", "quality-rank-arc-2");
 
         // Volatility Rank
-        document.getElementById("vol-rank").innerHTML = result.vol_rank;
-        document.getElementById("vol-rank-arc-1").style.borderColor = first_arc_border_color(result.vol_rank);
-        document.getElementById("vol-rank-arc-2").style.borderColor = second_arc_border_color(result.vol_rank);
-        document.getElementById("vol-rank-arc-2").style.transform =
-            "rotate(" + second_arc_transform(result.vol_rank) + "deg)";
-            
-        if (result.vol_rank === null) {
-            document.getElementById("vol-rank").innerHTML = 'n/a';
-            document.getElementById("vol-rank").style.color = '#898989';
-            document.getElementById("vol-rank-arc-1").style.borderColor = 'rgb(248,250,252)';
-            document.getElementById("vol-rank-arc-2").style.borderColor = 'rgb(248,250,252)';
-        }
-            
+        updateRank(result.vol_rank, "vol-rank", "vol-rank-arc-1", "vol-rank-arc-2");
             
         // Profitability Rank
-        document.getElementById("profit-rank").innerHTML = result.profit_rank;
-        document.getElementById("profit-rank-arc-1").style.borderColor = first_arc_border_color(result.profit_rank);
-        document.getElementById("profit-rank-arc-2").style.borderColor = second_arc_border_color(result.profit_rank);
-        document.getElementById("profit-rank-arc-2").style.transform =
-            "rotate(" + second_arc_transform(result.profit_rank) + "deg)";
-            
-        if (result.profit_rank === null) {
-            document.getElementById("profit-rank").innerHTML = 'n/a';
-            document.getElementById("profit-rank").style.color = '#898989';
-            document.getElementById("profit-rank-arc-1").style.borderColor = 'rgb(248,250,252)';
-            document.getElementById("profit-rank-arc-2").style.borderColor = 'rgb(248,250,252)';
-        }
-            
-            
-        // Financial Strength Rank
-        document.getElementById("fin-rank").innerHTML = result.finance_rank;
-        document.getElementById("fin-rank-arc-1").style.borderColor = first_arc_border_color(result.finance_rank);
-        document.getElementById("fin-rank-arc-2").style.borderColor = second_arc_border_color(result.finance_rank);
-        document.getElementById("fin-rank-arc-2").style.transform =
-            "rotate(" + second_arc_transform(result.finance_rank) + "deg)";
-            
-        if (result.finance_rank === null) {
-            document.getElementById("fin-rank").innerHTML = 'n/a';
-            document.getElementById("fin-rank").style.color = '#898989';
-            document.getElementById("fin-rank-arc-1").style.borderColor = 'rgb(248,250,252)';
-            document.getElementById("fin-rank-arc-2").style.borderColor = 'rgb(248,250,252)';
-        }
-            
-            
-        // Accounting Quality Rank
-        document.getElementById("safe-rank").innerHTML = result.safety_rank;
-        document.getElementById("safe-rank-arc-1").style.borderColor = first_arc_border_color(result.safety_rank);
-        document.getElementById("safe-rank-arc-2").style.borderColor = second_arc_border_color(result.safety_rank);
-        document.getElementById("safe-rank-arc-2").style.transform =
-            "rotate(" + second_arc_transform(result.safety_rank) + "deg)";
-            
-        if (result.safety_rank === null) {
-            document.getElementById("safe-rank").innerHTML = 'n/a';
-            document.getElementById("safe-rank").style.color = '#898989';
-            document.getElementById("safe-rank-arc-1").style.borderColor = 'rgb(248,250,252)';
-            document.getElementById("safe-rank-arc-2").style.borderColor = 'rgb(248,250,252)';
-        }
+        updateRank(result.profit_rank, "profit-rank", "profit-rank-arc-1", "profit-rank-arc-2");
+
+        // Financing Rank
+        updateRank(result.finance_rank, "fin-rank", "fin-rank-arc-1", "fin-rank-arc-2");
+
+        // Safety Rank
+        updateRank(result.safety_rank, "safe-rank", "safe-rank-arc-1", "safe-rank-arc-2");
+
 
 	})
 })
@@ -209,4 +118,19 @@ function second_arc_transform(rank) {
         degrees = 45;
     }
     return degrees;
+}
+
+function updateRank(rank, text_id, arc_id_1, arc_id_2) {
+	document.getElementById(text_id).innerHTML = rank;
+    document.getElementById(arc_id_1).style.borderColor = first_arc_border_color(rank);
+    document.getElementById(arc_id_2).style.borderColor = second_arc_border_color(rank);
+    document.getElementById(arc_id_2).style.transform =
+        "rotate(" + second_arc_transform(rank) + "deg)";
+
+    if (rank === null) {
+        document.getElementById(text_id).innerHTML = null_value;
+        document.getElementById(text_id).style.color = null_text_color;
+        document.getElementById(arc_id_1).style.borderColor = null_border_color;
+        document.getElementById(arc_id_2).style.borderColor = null_border_color;
+    }
 }
